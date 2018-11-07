bf80c114259a8268453a05594c4df340d7c374ed
kevingo <kevingo75@gmail.com>
2018-11-07 12:20:07 +0800

    Add example

---
+++ examples/mnist/main.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+def main():
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
